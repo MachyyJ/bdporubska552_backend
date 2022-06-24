fc5f52af24c43833b35c99ef7ad713d5b0f7d317
MachyyDev <jiri.machynka@mendelova-stredni.cz>
2022-06-25 00:32:20 +0200

    Added cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
 
 // BodyParser Middleware
 app.use(express.json())
+app.use(cors())
 
 // Routes
 const announcementRoutes = require('./routes/api/announcement')
